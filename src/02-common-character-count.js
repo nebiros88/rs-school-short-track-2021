62c827bf4bc2160ca4f5b8efdce5f0fe0abba757
nebiros88 <36847332+nebiros88@users.noreply.github.com>
2021-04-11 23:26:06 +0300

    feat: add solutions for task 1 - 3

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,45 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const str1 = s1.split('').sort();
+  const str2 = s2.split('').sort();
+  const arr1 = [];
+  const arr2 = [];
+  function getPars(str, arr) {
+    let counter = 1;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i + 1] === str[i]) {
+        counter++;
+      } else {
+        const temp = [str[i], counter];
+        arr.push(temp);
+        counter = 1;
+      }
+    }
+  }
+  function compareArrs(a, b) {
+    let result = 0;
+    const temp1 = (a.length > b.length) ? b : a;
+    const temp2 = (a.length > b.length) ? a : b;
+    temp1.forEach((el) => {
+      for (let i = 0; i < temp2.length; i++) {
+        if (el[0] === temp2[i][0]) {
+          if (el[1] === temp2[i][1]) {
+            result += el[1];
+          } else if (el[1] < temp2[i][1]) {
+            result += el[1];
+          } else {
+            result += temp2[i][1];
+          }
+        }
+      }
+    });
+    return result;
+  }
+  getPars(str1, arr1);
+  getPars(str2, arr2);
+  return compareArrs(arr1, arr2);
 }
 
 module.exports = getCommonCharacterCount;
--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,30 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const arrSize = arr.length;
+  const sortedArr = arr;
+  for (let i = 0; i < arrSize; i++) {
+    for (let j = 0; j < arrSize - 2; j++) {
+      if (sortedArr[j] === -1) {
+        while (sortedArr[j] === -1) {
+          j++;
+        }
+      }
+      let next = j + 1;
+      if (sortedArr[next] === -1) {
+        while (sortedArr[next] === -1) {
+          next++;
+        }
+      }
+      if (sortedArr[j] > sortedArr[next]) {
+        const temp = sortedArr[j];
+        sortedArr[j] = sortedArr[next];
+        sortedArr[next] = temp;
+      }
+    }
+  }
+  return sortedArr;
 }
 
 module.exports = sortByHeight;
